738d9c9197d512ecb176db66511ad154a7b83041
Nhanyichiukwu Hopeson Otuosorochiukwu <otuosorochihopeson@gmail.com>
2024-10-21 09:19:40 +0100

    Build time bug fix

--- app/(main)/team/[userId]/layout.tsx
+++ app/(main)/team/[userId]/layout.tsx
@@ -1,17 +1,13 @@
 "use client";
 
-import React, { useCallback, useEffect } from "react";
-import { usePathname } from "next/navigation";
+import React, { useEffect } from "react";
 import NotFoundPage from '@/app/not-found';
-import { useGetUserById, useGetUserProperties } from '@/hooks/useGetUsers';
-import NoNetwork from '@/components/no-network';
+import { useGetUserById } from '@/hooks/useGetUsers';
 import ProfileHeader from '@/components/profile/header';
 import ProfileCard from '@/components/profile/profile-card';
-import InternalErrorPage from '@/app/internal-error';
 import useAuthenticatedUser from '@/hooks/useAuthenticatedUser';
 import { cn } from '@/lib/utils';
 import { usePageHeader } from "@/hooks/usePageHeaderContext";
-import { useState } from "react";
 import Link from "next/link";
 import UserSearchForm from "@/components/dashboard/user-search-form";
 import { Text } from "@/components/ui/text";
@@ -29,37 +25,6 @@ export default function ProfileLayout({
 
     const { userId } = params;
     const [isLoading, user, error] = useGetUserById(userId, actor as AppUser);
-    // const [fallback, setFallback] = useState(<div className="text-center">Loading, please wait...</div>)
-
-
-
-    // useEffect(() => {
-    //     if (!isLoading) {
-    //         return;
-    //     }
-    //     if (error !== null) {
-    //         if (error.code) {
-    //             switch (error.code) {
-    //                 case 'ERR_NETWORK':
-    //                     setFallback(<NoNetwork />)
-    //                     break;
-    //                 default:
-    //                     break;
-    //             }
-    //         }
-    //         if (error.response && error.response.status == 404) {
-    //             // const res = error.response.data;
-    //             setFallback(<NotFoundPage text='Sorry, we could not find any user by the given id. You must have followed a broken link.' />);
-    //         }
-    //     } else if (user === null) {
-    //         console.error(error);
-    //         setFallback(<InternalErrorPage />);
-    //     }
-
-    //     return () => {
-
-    //     }
-    // }, [isLoading, user, error]);
 
     useEffect(() => {
         setPageTitle(null);
@@ -107,7 +72,7 @@ export default function ProfileLayout({
                 <div id='user-profile' className={cn('relative flex flex-col')}>
                     <header id='profile-header' className='flex flex-col header w-full'>
                         <div className='flex flex-col gap-3 relative px-4 lg:px-8'>
-                            <ProfileHeader userId={userId} />
+                            <ProfileHeader account={user} />
                         </div>
                     </header>
                     <main id='profile-body' className='px-4 lg:px-8'>
